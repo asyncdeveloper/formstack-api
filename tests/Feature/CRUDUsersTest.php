a8a81167f6a40b4f9bfbd422f23f9436910b929d
Oluwaseyi Adeogun <sheyilaaw98@gmail.com>
2020-06-08 18:10:59 +0100

    Setup PHPUnit

---
+++ tests/Feature/CRUDUsersTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+class CRUDUsersTest extends TestCase
+{
+
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+
+}
