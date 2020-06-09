f59b50a3bae03a7e0f72695bc75a32597dd15c46
Oluwaseyi Adeogun <sheyilaaw98@gmail.com>
2020-06-09 12:34:01 +0100

    Add route for home page

--- routes.php
+++ routes.php
@@ -1,11 +1,20 @@
 <?php
 
+use App\Handlers\CustomExceptionHandler;
 use Pecee\SimpleRouter\SimpleRouter;
 
 SimpleRouter::setDefaultNamespace('\App\Controllers');
 
-SimpleRouter::group(['prefix' => '/api', 'exceptionHandler' => \App\Handlers\CustomExceptionHandler::class ], function () {
-    SimpleRouter::resource('/users', 'UserController');
+SimpleRouter::group(['exceptionHandler' => CustomExceptionHandler::class], function () {
+    SimpleRouter::get('/', function () {
+        return response()->json([
+            'message' => 'Welcome to PHP Rest API visit api/users'
+        ]);
+    });
+
+    SimpleRouter::group(['prefix' => 'api'], function () {
+        SimpleRouter::resource('/users', 'UserController');
+    });
 });
 
 SimpleRouter::start();
